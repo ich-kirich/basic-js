17ff8653f52ab1c0a5c25218eb6f0fa40041f046
ich_kirich <99396626+ich-kirich@users.noreply.github.com>
2022-10-16 17:07:29 +0300

    feat: make a task transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,46 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if(Array.isArray(arr)){
+    res = []
+    j = 0
+    commands = ['--discard-next', '--discard-prev', '--double-next', '--double-prev']
+    for(let i = 0; i < arr.length; i++){
+      if(arr[i] === '--discard-next'){
+        if(!commands.includes(arr[i+1]) && (i+1 < arr.length)){
+          i++
+        }
+      }
+      else if(arr[i] === '--discard-prev'){
+        if(i - 1 >= 0 && !commands.includes(arr[i-1]) && arr[i-2] !== '--discard-next'){
+          j--
+          res.splice(j, 1);
+          j = res.length - 1
+        }
+      }
+      else if(arr[i] === '--double-next'){
+        if((i + 1) < arr.length && !commands.includes(arr[i+1])){
+          res.push(arr[i + 1])
+          j = res.length - 1
+        }
+      }
+      else if(arr[i] === '--double-prev'){
+        if((i - 1) >= 0 && !commands.includes(arr[i-1]) && arr[i-2] !== '--discard-next'){
+          res.push(arr[i-1])
+          j = res.length - 1
+        }
+      }
+      else{
+        res.push(arr[i])
+        j = res.length - 1
+      }
+    }
+    return res
+  }
+  else{
+    throw new Error('\'arr\' parameter must be an instance of the Array!')
+  }
 }
 
 module.exports = {
