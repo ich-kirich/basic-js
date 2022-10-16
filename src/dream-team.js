1017811d6f5838497b04c8fff29808178b3c2599
ich_kirich <99396626+ich-kirich@users.noreply.github.com>
2022-10-16 14:41:56 +0300

    feat: make task dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  let s = ""
+  if(members === "" || !isNaN(members) || (members === Infinity || members === -Infinity) || members === undefined || !Array.isArray(members)){
+    return false
+  }
+  for(let j = 0; j < members.length; j++){
+    if(members[j] !== null && isNaN(members[j]) && !(members[j] === Infinity || members[j] === -Infinity) && members[j] !== undefined && typeof members[j] === 'string'){
+      members[j] = members[j].trim()
+      members[j] = members[j].toUpperCase() 
+    }
+  }
+  members = members.sort()
+  for(let i = 0; i < members.length; i++){
+    if(typeof members[i] === 'string' && isNaN(members[i]) && !(members[i] === Infinity || members[i] === -Infinity) && members[i] !== undefined && members[i] !== null){
+      if(isNaN(members[i][0])){
+        s += members[i][0].toUpperCase() 
+      }
+    }
+  }
+  if(s === ""){
+    return false
+  }
+  return s
 }
 
 module.exports = {
